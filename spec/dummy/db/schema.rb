9ff6f4721c290aaf72732bc9bc7ff880d8520e51
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-12-08 19:28:32 -0200

    Removed duplicated migrations

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206162236) do
+ActiveRecord::Schema.define(version: 20131206162038) do
 
   create_table "gossiper_notifications", force: true do |t|
     t.integer  "user_id"
