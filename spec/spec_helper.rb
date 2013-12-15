4eef76267187491d097612071fc4af1e9efe449d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-12-14 22:25:35 -0200

    Coverage with coveralls only when travis executes it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'coveralls'
-Coveralls.wear!
-
 ENV["RAILS_ENV"] ||= 'test'
-require 'simplecov'
 
-SimpleCov.start 'rails' do
+if ENV['TRAVIS']
+  require 'coveralls'
+  Coveralls.wear!('rails')
 end
+
+require 'simplecov'
+SimpleCov.start 'rails'
 
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
