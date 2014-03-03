758a458a435f02bc5f19f3c28d380af9445af636
Marcelo <marcelo.jacobus@gmail.com>
2014-03-03 15:48:35 -0300

    Update 20131214081503_add_data_to_notifications.rb

--- db/migrate/20131214081503_add_data_to_notifications.rb
+++ db/migrate/20131214081503_add_data_to_notifications.rb
@@ -1,5 +1,5 @@
 class AddDataToNotifications < ActiveRecord::Migration
   def change
-    add_column :gossiper_notifications, :data, :test
+    add_column :gossiper_notifications, :data, :text
   end
 end
